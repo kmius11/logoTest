933a16be324a4ad514f470900a42d8f75d8da800
EinerDiaz <64668143+EinerDiaz@users.noreply.github.com>
2021-04-28 22:10:58 -0500

    ACTUALIZACION

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+import cv2
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "ESTO ES UNA PRUEBA GOGO!"
+
+def index():
+   
+    return render_template("index.html")
+
+@app.route("/servicio",methods=["POST"])
+def servicio():
+    usuario=request.form.get("nombre")
+    return render_template("servicio.html",nombre=usuario)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
     
